857460fca281d08a7d4cf0f7ecb08630835ad527
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-18 23:54:19 +0530

    Add browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Orders from './pages/Orders';
+import Products from './pages/Products';
+
+import './App.css';
 
 const App = () => {
 	return (
-		<div className="App">
-			Hello World!
+		<div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' exact element={<Home />} />
+					<Route path='/orders' element={<Orders />} />
+					<Route path='/products' element={<Products />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
