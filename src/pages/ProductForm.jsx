b1f59fd5f234d2e9e302e3f30ab7d7b2a6ca1c28
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-19 20:05:11 +0530

    Update default values

--- src/pages/ProductForm.jsx
+++ src/pages/ProductForm.jsx
@@ -1,45 +1,54 @@
-import Navbar from "../components/Navbar";
+import { useLocation } from 'react-router-dom';
+
+import Navbar from '../components/Navbar';
 
 const ProductForm = () => {
+
+    const location = useLocation();
+    const action = location.pathname.split('/')[2].charAt(0).toUpperCase() + location.pathname.split('/')[2].slice(1);
+
+    const handleSubmit = () => {
+        
+    }
+
+    console.log(location);
     return (
         <div className='ProductForm'>
-            <Navbar/>
-             <div className="container align-items-center">
-                <h2 className="my-4">Add Product</h2>
-                <form>
-                    
-                    <div className="mb-3">
-                        <label htmlFor="fullname" className="form-label">Name</label>
-                        <input type="text" name="fullname" className="form-control" />
+            <Navbar />
+            <div className='container'>
+                <h2 className='my-3'>{action} Product</h2>
+                <form className='p-3 border rounded'>
+                    <div className='mb-3'>
+                        <label className='form-label' htmlFor='product_name'>Name</label>
+                        <input className='form-control' type='text' name='product_name' id='product_name' defaultValue={location.state?.name} />
                     </div>
-                    <div className="input-group my-4">
-                        <select className="form-select" name="category">
-                            <option hidden>Category</option>
-                            <option value="Snacks">Snacks</option>
-                            <option value="Meals">Meals</option>
-                            <option value="Beverages">Beverages</option>
-                            <option value="Others">Others</option>
-                          </select>
+                    <div className='mb-3'>
+                        <label className='form-label' htmlFor='rate'>Rate</label>
+                        <input className='form-control' type='number' name='rate' id='rate' defaultValue={location.state?.rate} />
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="contact" className="form-label">Price</label>
-                        <input type="text" name="Price" className="form-control" />
+                    <label htmlFor="category" className="form-label">Category</label>
+                    <div className='input-group mb-3'>
+                        <select className='form-select' name='category' id='category' defaultValue={location.state?.category}>
+                            <option hidden disabled></option>
+                            <option value='snack'>Snack</option>
+                            <option value='meal'>Meal</option>
+                            <option value='beverage'>Beverage</option>
+                            <option value='other'>Other</option>
+                        </select>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="wesite" className="form-label">Description</label>
-                        <textarea className="form-control" name="subject" placeholder="Write something.." ></textarea>
+                    <div className='mb-3'>
+                        <label htmlFor="imgUrl">Image URL</label>
+                        <input className='form-control' type="text" name='imgUrl' id='imgUrl' defaultValue={location.state?.imgUrl} />
                     </div>
-                    <div className="mb-3">
-                    
-                        <input type="file" className="form-control-file" />
+                    <div className='mb-3'>
+                        <label htmlFor='description' className='form-label'>Description</label>
+                        <textarea className='form-control' name='description' id='description' defaultValue={location.state?.description}></textarea>
                     </div>
-                    <button type="submit" className="btn btn-primary mb-4">
-                        Submit
-                    </button>
+                    <button className='btn btn-primary' onClick={handleSubmit()}>{action}</button>
                 </form>
             </div>
         </div>
-            )
-        }
+    );
+}
 
-    export default ProductForm;
+export default ProductForm;
