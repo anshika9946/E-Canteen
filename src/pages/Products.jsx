8125d5c0ed27d2e96a3d3a0bd63bbdedd4a7a2bb
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-19 10:48:37 +0530

    Add products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,45 @@
-import Navbar from "../components/Navbar";
+import { useState } from 'react';
+
+import useFetch from '../hooks/useFetch';
+import Navbar from '../components/Navbar';
+import ProductCard from '../components/ProductCard';
 
 const Products = () => {
+
+    const [category, setCategory] = useState('');
+
+    const { response } = useFetch(`/api/v1/product/${category}`);
+
     return (
-        <div className="Products">
+        <div className='Products'>
             <Navbar />
-            <h1>This is products page</h1>
+            <div className='container my-3'>
+                <h2>Products</h2>
+                <div className="d-flex justify-content-between py-3">
+                    <ul className='nav nav-pills'>
+                        <li className='nav-item'><button className={`btn btn${category === '' ? '' : '-outline'}-primary me-2`} onClick={() => setCategory('')}>All</button></li>
+                        <li className='nav-item'><button className={`btn btn${category === 'snack' ? '' : '-outline'}-primary me-2`} onClick={() => setCategory('snack')}>Snacks</button></li>
+                        <li className='nav-item'><button className={`btn btn${category === 'meal' ? '' : '-outline'}-primary me-2`} onClick={() => setCategory('meal')}>Meals</button></li>
+                        <li className='nav-item'><button className={`btn btn${category === 'beverage' ? '' : '-outline'}-primary me-2`} onClick={() => setCategory('beverage')}>Beverages</button></li>
+                    </ul>
+                    <div><input className='form-control' type='search' placeholder='Search' /></div>
+                </div>
+
+                {response?.ok && response.data?.map((product => {
+
+                    const { _id, product_name, rate, available, category, createdAt, updatedAt } = product;
+
+                    return <ProductCard
+                        key={_id}
+                        name={product_name}
+                        rate={rate}
+                        available={available}
+                        category={category}
+                        createdAt={createdAt}
+                        updatedAt={updatedAt}
+                    />
+                }))}
+            </div>
         </div>
     );
 }
