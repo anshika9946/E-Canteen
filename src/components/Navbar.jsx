5bd559dbae5fae610e56257d737cb47d57277f70
Anshika <anshika2011087@akgec.ac.in>
2023-02-19 09:44:49 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,30 @@
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
         <div className='Navbar'>
-            <nav className='navbar navbar-expand-lg bg-light'>
+            <nav className='navbar navbar-expand-lg bg-warning '>
                 <div className='container-fluid'>
-                    <a className='navbar-brand' href='#'>
-                        <img src='/docs/5.2/assets/brand/bootstrap-logo.svg' alt='Logo' width='30' height='24' className='d-inline-block align-text-top' />
-                        E-Canteen Admin
-                    </a>
+                    <NavLink to="/" className='navbar-brand'>
+                        <img src='\src\images\logo.png' alt='Logo' width='80' height='60' className='d-inline-block align-text-top' />
+                       
+                    </NavLink>
                     <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
                         <span className='navbar-toggler-icon'></span>
                     </button>
                     <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                            <li className='nav-item'>
-                                <a className='nav-link active' aria-current='page' href='#'>Home</a>
-                            </li>
-                            <li className='nav-item dropdown'>
-                                <a className='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
-                                    Dropdown
-                                </a>
-                                <ul className='dropdown-menu'>
-                                    <li><a className='dropdown-item' href='#'>Action</a></li>
-                                    <li><a className='dropdown-item' href='#'>Another action</a></li>
-                                    <li><hr className='dropdown-divider' /></li>
-                                    <li><a className='dropdown-item' href='#'>Something else here</a></li>
-                                </ul>
+                        <ul className='navbar-nav me-auto mb-2  mb-lg-0 '>
+                            <li className='nav-item '>
+                                <h5><NavLink className="nav-link active text-white " to="/" aria-current='page'>Home</NavLink></h5>
                             </li>
                             <li className='nav-item'>
-                                <a className='nav-link disabled'>Disabled</a>
+                                <h5><NavLink className="nav-link active text-white" to="/products" aria-current='page'>Products</NavLink></h5>
+                            </li>
+                            <li className='nav-item'>
+                                <h5><NavLink className="nav-link active text-white" to="/orders" aria-current='page'>Orders</NavLink></h5>
                             </li>
                         </ul>
                         <form className='d-flex' role='search'>
-                            <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
-                            <button className='btn btn-outline-success' type='submit'>Search</button>
+                        <h5><NavLink className="nav-link active text-white" to="/" aria-current='page'>Admin</NavLink></h5>
                         </form>
                     </div>
                 </div>
