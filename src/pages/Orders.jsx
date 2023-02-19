ea387ec8e10ca36cd43ed1642852bfbbe393b837
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-19 14:54:47 +0530

    Create orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,10 +1,46 @@
-import Navbar from "../components/Navbar";
+import { useState } from 'react';
+
+import useFetch from '../hooks/useFetch';
+import Navbar from '../components/Navbar';
+import OrderCard from '../components/OrderCard';
 
 const Orders = () => {
+
+    const [delivered, setDelivered] = useState('false');
+
+    const { response } = useFetch(`/admin/v1/order/${delivered}`);
+
     return (
-        <div className="Orders">
+        <div className='Orders'>
             <Navbar />
-            <h1>This is orders page</h1>
+            <div className='container my-3'>
+                <h2>Orders</h2>
+                <div className='d-flex justify-content-between py-3'>
+                    <ul className='nav'>
+                        <li className='nav-item'><button className={`btn btn${delivered === '' ? '' : '-outline'}-primary me-2`} onClick={() => setDelivered('')}>All</button></li>
+                        <li className='nav-item'><button className={`btn btn${delivered === 'false' ? '' : '-outline'}-primary me-2`} onClick={() => setDelivered('false')}>Pending</button></li>
+                        <li className='nav-item'><button className={`btn btn${delivered === 'true' ? '' : '-outline'}-primary me-2`} onClick={() => setDelivered('true')}>Delivered</button></li>
+                    </ul>
+                    <ul className='nav'>
+                        <li className='nav-item'><input className='form-control' type='search' placeholder='Search' /></li>
+                    </ul>
+                </div>
+                {response?.ok && response.data?.map(order => {
+
+                    const { _id, user_id, products, amount, delivered, token, createdAt, updatedAt } = order;
+
+                    return <OrderCard
+                        key={_id}
+                        user={user_id}
+                        products={products}
+                        amount={amount}
+                        delivered={delivered}
+                        token={token}
+                        createdAt={createdAt}
+                        updatedAt={updatedAt}
+                    />
+                })}
+            </div>
         </div>
     );
 }
