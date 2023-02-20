3b10fd1eb64463867340859498d3042ae112992e
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-20 10:56:30 +0530

    Add product available component

---
+++ src/pages/ProductAvailable.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const ProductAvailable = () => {
+    return (
+        <div className="ProductAvailable">
+            <Navbar />
+        </div>
+    );
+}
+
+export default ProductAvailable;
