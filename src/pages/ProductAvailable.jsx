bfa5b327669e3b6d4bb1d3060938131b4b44f31c
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-20 22:14:29 +0530

    Create page add filters

--- src/pages/ProductAvailable.jsx
+++ src/pages/ProductAvailable.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+
+import useFetch from "../hooks/useFetch";
 import Navbar from "../components/Navbar";
+import ProductAvailableCard from "../components/ProductAvailableCard";
 
 const ProductAvailable = () => {
+
+    const { response } = useFetch('/api/v1/product');
+
+    const [available, setAvailable] = useState(null);
+
     return (
         <div className="ProductAvailable">
             <Navbar />
+            <div className="container my-3">
+                <h2>Update Availability</h2>
+                <div className='d-flex justify-content-between py-3'>
+                    <ul className='nav'>
+                        <li className='nav-item'><button className={`btn btn${available === null ? '' : '-outline'}-primary me-2`} onClick={() => setAvailable(null)}>All</button></li>
+                        <li className='nav-item'><button className={`btn btn${available === true ? '' : '-outline'}-primary me-2`} onClick={() => setAvailable(true)}>Active</button></li>
+                        <li className='nav-item'><button className={`btn btn${available === false ? '' : '-outline'}-primary me-2`} onClick={() => setAvailable(false)}>Closed</button></li>
+                    </ul>
+                    <ul className='nav'>
+                        <li className='nav-item'><input className='form-control' type='search' placeholder='Search' /></li>
+                    </ul>
+                </div>
+
+                {response?.ok && response.data.filter(product => available === null ? true : product.available === available).map(product => {
+
+                    const { _id, product_name, rate, available, category, imageUrl, description, createdAt, updatedAt } = product;
+
+                    return <ProductAvailableCard
+                        key={_id}
+                        id={_id}
+                        name={product_name}
+                        rate={rate}
+                        available={available}
+                        category={category}
+                        imageUrl={imageUrl}
+                        description={description}
+                        createdAt={createdAt}
+                        updatedAt={updatedAt}
+                    />
+                })}
+            </div>
         </div>
     );
 }
